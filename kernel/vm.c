5dd5064ad5c5a5802050210be82417e8cd4c883a
Baokker <1048098862@qq.com>
2022-07-09 11:11:21 +0800

    finish print a pagetable

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,33 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// simulate freewalk to print vm
+void printwalk(pagetable_t pagetable, int depth)
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      for (int j = 0;j < depth;j++){
+        printf("..");
+        if (j != depth - 1)
+          printf(" ");
+      }
+      // this PTE points to a lower-level page table.
+      // type cast
+      uint64 child = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n", i, pte, child);
+
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0){
+        printwalk((pagetable_t)child, depth + 1);
+      }
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  printwalk(pagetable, 1);
+}
+
