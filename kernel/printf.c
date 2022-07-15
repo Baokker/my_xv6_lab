1aa84d90c7fec0124132d1a5877d771dc148f4bd
Baokker <1048098862@qq.com>
2022-07-15 22:07:20 +0800

    finish backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,12 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void backtrace(void){
+  printf("backtrace:\n");
+  uint64 fp = r_fp();
+  while (PGROUNDDOWN(fp) < PGROUNDUP(fp)){
+    printf("%p\n", *(uint64*)(fp - 8));
+    fp = *(uint64*)(fp - 16);
+  }
+}
