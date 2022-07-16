2ba46818c47eb8536ada745253d5c3b478b27b73
Baokker <1048098862@qq.com>
2022-07-16 10:53:32 +0800

    just delete some sbrk code hh

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,11 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  // if(growproc(n) < 0)
+  //   return -1;
+  // return addr;
+
+  myproc()->sz += n;
   return addr;
 }
 
